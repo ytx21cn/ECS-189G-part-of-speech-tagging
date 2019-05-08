85222ed9553afaf5a4935d0bc6fb8125f3b63c7a
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-08 09:03:00 -0700

    modified running script for task 2, as well as added results for tasks 2 and 3

--- hw2/task2.sh
+++ hw2/task2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # train a bigram hmm tagger
-perl ./train_hmm.pl ptb.2-21.tgs ptb.2-21.txt > my.hmm
+perl ./train_hmm.pl ptb.2-21.tgs ptb.2-21.txt > my-bigram.hmm
 
 # run the Viterbi algorithm to tag some data
-python2 ./viterbi.py my.hmm ptb.22.txt > my.out
+python2 ./viterbi.py my.hmm ptb.22.txt > my-bigram.out
 
 # evaluate
-perl ./tag_acc.pl ptb.22.tgs my.out
+perl ./tag_acc.pl ptb.22.tgs my-bigram.out
