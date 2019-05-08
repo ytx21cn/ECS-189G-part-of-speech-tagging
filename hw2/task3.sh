0f1d35fa023f535006291307bdd240a7a5a670eb
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-08 09:09:41 -0700

    running script for task 3

---
+++ hw2/task3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# train a trigram hmm tagger
+python2 ./train_hmm_trigram.py ptb.2-21.tgs ptb.2-21.txt > my-trigram.hmm
+
+# run the Viterbi algorithm to tag some data
+python2 ./viterbi-trigram.py my.hmm ptb.22.txt > my-trigram.out
+
+# evaluate
+perl ./tag_acc.pl ptb.22.tgs my-trigram.out
