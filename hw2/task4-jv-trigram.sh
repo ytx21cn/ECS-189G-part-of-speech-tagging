4a87ea5ba43a04ffef63f65d382d14e70c9e0f3a
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-08 21:40:09 -0700

    added task 4 jv trigram code and scripts

--- hw2/task4-jv-trigram.sh
+++ hw2/task4-jv-trigram.sh
@@ -4,7 +4,7 @@
 python2 ./train_hmm_trigram.py jv.train.tgs jv.train.txt > my-jv-trigram.hmm
 
 # run the Viterbi algorithm to tag some data
-python2 ./viterbi-trigram.py my-jv-trigram.hmm jv.test.txt > my-jv-trigram.out
+python2 ./viterbi-trigram-jv.py my-jv-trigram.hmm jv.test.txt > my-jv-trigram.out
 
 # evaluate
 perl ./tag_acc.pl jv.test.tgs my-jv-trigram.out
