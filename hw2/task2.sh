61f54b1d6723d3d0d0e11c1db58cf4b52445a80b
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-05-08 17:32:11 -0700

    updated sh files for task 2 and 3

--- hw2/task2.sh
+++ hw2/task2.sh
@@ -4,7 +4,7 @@
 perl ./train_hmm.pl ptb.2-21.tgs ptb.2-21.txt > my-bigram.hmm
 
 # run the Viterbi algorithm to tag some data
-python2 ./viterbi.py my.hmm ptb.22.txt > my-bigram.out
+python2 ./viterbi.py my-bigram.hmm ptb.22.txt > my-bigram.out
 
 # evaluate
 perl ./tag_acc.pl ptb.22.tgs my-bigram.out
--- hw2/task3.sh
+++ hw2/task3.sh
@@ -4,7 +4,7 @@
 python2 ./train_hmm_trigram.py ptb.2-21.tgs ptb.2-21.txt > my-trigram.hmm
 
 # run the Viterbi algorithm to tag some data
-python2 ./viterbi-trigram.py my.hmm ptb.22.txt > my-trigram.out
+python2 ./viterbi-trigram.py my-trigram.hmm ptb.22.txt > my-trigram.out
 
 # evaluate
 perl ./tag_acc.pl ptb.22.tgs my-trigram.out
